c989acfb5fefe2ad5f083a55a701af01f856dc05
misstime <734487946@qq.com>
2022-11-15 22:11:59 +0800

    first commit: 1.0 beta

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# pbtidy
-一个小工具，用于修剪 proto 文件编译后的 pb.go 文件。
+pbtidy
+=======
+
+一个小工具，用于修剪 proto 文件编译后的后缀为 `.pb.go` 的文件，使之可以直接使用 [validator库](https://github.com/go-playground/validator)。
+
+例如，proto 文件如下：  
+
+```protobuf
+// ...
+
+message Person {
+  string name = 1; // 姓名 @{required}
+  int64 age = 2; // 年龄 @{required,max=20,min=6} @{这里不会替换}
+  bool gender = 3; // 性别
+}
+
+// ...
+```
+
+`protoc` 使用 golang 插件对 proto 文件编译后的 `.pb.go` 文件如下：  
+
+```go
+// ...
+
+type Person struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Name   string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`      // 姓名 @{required}
+	Age    int64  `protobuf:"varint,2,opt,name=age,proto3" json:"age,omitempty"`       // 年龄 @{required,max=20,min=6} @{这里不会替换}
+	Gender bool   `protobuf:"varint,3,opt,name=gender,proto3" json:"gender,omitempty"` // 性别
+}
+
+// ...
+```
+
+使用本工具对 .pb.go 文件修剪后的效果如下：  
+
+```go 
+// ...
+
+type Person struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Name   string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty" validate:"required"`      // 姓名 
+	Age    int64  `protobuf:"varint,2,opt,name=age,proto3" json:"age,omitempty" validate:"required,max=20,min=6"`       // 年龄  @{这里不会替换}
+	Gender bool   `protobuf:"varint,3,opt,name=gender,proto3" json:"gender,omitempty"` // 性别
+}
+
+// ...
+```
+
+Usage && Doc
+------------
+
+example:  
+
+```shell
+$ pbtidy -dir="/data/testdata" -rec=false
+```
+
+doc:  
+```shell
+$ pbtidy --help
+
+>  -dir string                              
+>        指定的目录                         
+>  -rec                                     
+>        是否递归处理子文件夹 (default true)
+```
+
+
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"regexp"
+	"time"
+)
+
+const (
+	leftDelimiter  = "@{"
+	rightDelimiter = "}"
+)
+
+var (
+	reg1 = regexp.MustCompile(".+`protobuf:\".+?\" json:\".+?\"`\\s*//.*?" + leftDelimiter + ".+?" + rightDelimiter + ".*")           // 待修正行正则
+	reg2 = regexp.MustCompile("(.+`protobuf:\".+?\" json:\".+?\"`)(\\s*//.*?)(" + leftDelimiter + "(.+?)" + rightDelimiter + ")(.*)") // reg2同reg1，仅多出子匹配
+)
+
+// getPbFiles 获取指定文件夹下所有后缀为 .pb.go 的文件路径。
+// dirPath 为入口根目录，rec 是否递归处理子文件夹，filePath 保存返回的文件路径
+func getPbFiles(dirPath string, rec bool, filePaths *[]string) error {
+	rd, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return err
+	}
+	for _, info := range rd {
+		if info.IsDir() {
+			if rec {
+				err := getPbFiles(dirPath+"/"+info.Name(), rec, filePaths)
+				if err != nil {
+					return err
+				}
+			}
+		} else {
+			fileName := info.Name()
+			if len(fileName) >= 6 && fileName[len(fileName)-6:] == ".pb.go" {
+				*filePaths = append(*filePaths, dirPath+"/"+info.Name())
+			}
+		}
+	}
+	return nil
+}
+
+// tidyPbFile 对文件中的每一行进行正则处理，编辑为期望样式。done 通道用以在main协程中实现超时控制。
+func tidyPbFile(filePath string, done chan<- struct{}) {
+	fileBytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := bytes.Split(fileBytes, []byte("\n"))
+	for k, line := range lines {
+		if !reg1.Match(line) {
+			continue
+		}
+		subs := reg2.FindSubmatch(line)
+		newSub1 := [][]byte{
+			subs[1][:len(subs[1])-1],
+			[]byte(" validate:\""),
+			subs[4],
+			[]byte("\"`"),
+		}
+		subs[1] = bytes.Join(newSub1, nil)
+		newSubs := [][]byte{
+			subs[1],
+			subs[2],
+			subs[5],
+		}
+		lines[k] = bytes.Join(newSubs, nil)
+	}
+	newFileBytes := bytes.Join(lines, []byte("\n"))
+	err = ioutil.WriteFile(filePath, newFileBytes, fs.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	done <- struct{}{}
+}
+
+func main() {
+	// 命令行参数解析
+	var (
+		dir string
+		rec bool
+	)
+	flag.StringVar(&dir, "dir", "", "指定的目录")
+	flag.BoolVar(&rec, "rec", true, "是否递归处理子文件夹")
+	flag.Parse()
+
+	// 获取所有 .pb.go 文件路径
+	filePaths := make([]string, 0)
+	err := getPbFiles(dir, rec, &filePaths)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(filePaths) == 0 {
+		log.Fatal("pbtidy: find none `.pb.go` files")
+	}
+
+	// 并发处理所有任务。当所任务处理完毕 或 超时，main协程退出
+	var (
+		done    = make(chan struct{}, len(filePaths))
+		timeout = time.After(time.Second * 3)
+		doneCnt = 0
+	)
+	for _, filePath := range filePaths {
+		go tidyPbFile(filePath, done)
+	}
+	for {
+		select {
+		case <-timeout:
+			log.Fatalf("pbtidy timeout: timeout goroutine count -- %v", len(filePaths)-doneCnt)
+		case <-done:
+			doneCnt++
+			if doneCnt == len(filePaths) {
+				log.Println("pbtidy done")
+				return
+			}
+		}
+	}
+}
